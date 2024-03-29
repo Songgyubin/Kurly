4a1a3ead1cea94c101c9f54d9034362942b3f87b
songgyubin <thd0427__@naver.com>
2024-03-29 18:19:16 +0900

    📦️ Section 별 상품 리스트 api 추가

--- core/network/src/main/java/com/gyub/core/network/retrofit/SectionApiService.kt
+++ core/network/src/main/java/com/gyub/core/network/retrofit/SectionApiService.kt
@@ -1,6 +1,8 @@
 package com.gyub.core.network.retrofit
 
 import com.gyub.core.network.const.EndPoints.SECTIONS
+import com.gyub.core.network.const.EndPoints.SECTION_PRODUCTS
+import com.gyub.core.network.model.Product
 import com.gyub.core.network.model.Section
 import com.gyub.core.network.model.base.NetworkResponse
 import retrofit2.http.GET
@@ -19,5 +21,11 @@ interface SectionApiService {
      */
     @GET(SECTIONS)
     suspend fun getSections(@Query("page") page: Int): NetworkResponse<List<Section>>
+
+    /**
+     * Section 별 상품 리스트 가져오기
+     */
+    @GET(SECTION_PRODUCTS)
+    suspend fun getSectionProducts(@Query("sectionId") sectionId: Int): NetworkResponse<List<Product>>
 
 }
