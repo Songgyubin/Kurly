c8da288aae3f65a340e81dae7a265dc5755f001f
gyu_b <thd0427__@naver.com>
2024-03-29 15:59:20 +0900

    🏗️ data 모듈 추가 (#6)
    * data 모듈 추가
    
    * data model 추가
    
    * repository 구현체 추가
    
    * repository di 추가

---
+++ core/data/src/main/java/repository/SectionRepositoryImpl.kt
@@ -0,0 +1,27 @@
+package repository
+
+import com.gyub.core.domain.model.SectionEntity
+import com.gyub.core.domain.repository.SectionRepository
+import com.gyub.core.network.datasource.SectionsDataSource
+import model.toEntity
+import javax.inject.Inject
+
+/**
+ * Section Repository 구현체
+ *
+ * @author   Gyub
+ * @created  2024/03/29
+ */
+class SectionRepositoryImpl @Inject constructor(
+    private val sectionsDataSource: SectionsDataSource
+) : SectionRepository {
+
+    /**
+     * 각 섹션 가져오기
+     */
+    override suspend fun getSections(page: Int): List<SectionEntity> {
+        return sectionsDataSource.getSections(page).map {
+            it.toEntity()
+        }
+    }
+}
