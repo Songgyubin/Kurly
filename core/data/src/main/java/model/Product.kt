cd89f157d7acd5fd93716862179fb75a4df9eea8
songgyubin <thd0427__@naver.com>
2024-03-29 18:31:22 +0900

    📦️ 상품 리스트 데이터 소스 및 repository 추가

---
+++ core/data/src/main/java/model/Product.kt
@@ -0,0 +1,21 @@
+package model
+
+import com.gyub.core.domain.model.ProductEntity
+import com.gyub.core.network.model.Product
+
+/**
+ * [Product] to [ProductEntity]
+ *
+ * @author   Gyub
+ * @created  2024/03/29
+ */
+fun Product.toEntity(): ProductEntity {
+    return ProductEntity(
+        id = id,
+        name = name,
+        image = image,
+        originalPrice = originalPrice,
+        discountedPrice = discountedPrice,
+        isSoldOut = isSoldOut
+    )
+}
