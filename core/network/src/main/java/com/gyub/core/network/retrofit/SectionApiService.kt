426df57faae4c6c483a672be62b1cf3fbcd0f0bc
gyu_b <thd0427__@naver.com>
2024-03-29 14:41:05 +0900

    ✅ 응답 모델 구성 및 Json 파일을 통한 테스트 완료 (#2)
    * mock Server 세팅
    
    * fake data source 테스트 코드 작성
    
    * 응답 모델 구성
    
    * 응답 모델 Test 코드 추가

---
+++ core/network/src/main/java/com/gyub/core/network/retrofit/SectionApiService.kt
@@ -0,0 +1,11 @@
+package com.gyub.core.network.retrofit
+
+/**
+ *
+ *
+ * @author   Gyub
+ * @created  2024/03/28
+ */
+interface SectionApiService {
+
+}
---
+++ core/network/src/main/java/com/gyub/core/network/util/extension/GsonExtension.kt
@@ -0,0 +1,18 @@
+package com.gyub.core.network.util.extension
+
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+/**
+ *
+ *
+ * @author   Gyub
+ * @created  2024/03/29
+ */
+
+/**
+ * Json으로부터 [T] 추출
+ */
+inline fun <reified T> Gson.fromJson(json: String): T? {
+    return fromJson(json, object : TypeToken<T>() {}.type)
+}
