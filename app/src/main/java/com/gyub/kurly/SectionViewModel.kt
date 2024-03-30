a132af9b8c599a559ad032037a3dfaa97c27779f
gyu_b <thd0427__@naver.com>
2024-03-30 17:10:25 +0900

    📦️ 메인 화면 및 뷰모델 기초 세팅 (#10)
    * 📦️ MainActivity 추가
    
    * ➕️ dataBinding 추가
    
    * Activity, 뷰모델, 데이터 바인딩 세팅

---
+++ app/src/main/java/com/gyub/kurly/SectionViewModel.kt
@@ -0,0 +1,18 @@
+package com.gyub.kurly
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+/**
+ *
+ *
+ * @author   Gyub
+ * @created  2024/03/30
+ */
+@HiltViewModel
+class SectionViewModel @Inject constructor(
+
+): ViewModel() {
+
+}
