4680c65c4bfc7ebcea2102b5dd5f5adb0413a329
songgyubin <thd0427__@naver.com>
2024-03-30 16:57:43 +0900

    📦️ Application 세팅

---
+++ app/src/main/java/com/gyub/kurly/KurlyApplication.kt
@@ -0,0 +1,13 @@
+package com.gyub.kurly
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Kurly Application
+ *
+ * @author   Gyub
+ * @created  2024/03/30
+ */
+@HiltAndroidApp
+class KurlyApplication : Application()
