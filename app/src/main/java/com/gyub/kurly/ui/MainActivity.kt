4eafe7a4c871e7c39baadbd81211bbefae6c4991
songgyubin <thd0427__@naver.com>
2024-03-30 17:05:27 +0900

    Activity, 뷰모델, 데이터 바인딩 세팅

--- app/src/main/java/com/gyub/kurly/ui/MainActivity.kt
+++ app/src/main/java/com/gyub/kurly/ui/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.gyub.kurly.ui
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.gyub.kurly.R
+import com.gyub.kurly.SectionViewModel
+import com.gyub.kurly.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
@@ -13,6 +16,13 @@ import dagger.hilt.android.AndroidEntryPoint
  */
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val sectionViewModel: SectionViewModel by viewModels()
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater).apply {
+            lifecycleOwner = this@MainActivity
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
